1b0a31e7ec383878ec586af158f1245ee8bdb5ca
ksobczyk_R2DS <ksobczyk@student.agh.edu.pl>
2025-02-23 11:28:51 +0100

    feat: replace mape with mase

--- utils/calculate_metrics.py
+++ utils/calculate_metrics.py
@@ -12,11 +12,12 @@ def calculate_metrics(y_true, y_pred):
     # R-squared (R2)
     r2 = r2_score(y_true, y_pred)
 
-    # Mean Absolute Percentage Error (MAPE)
-    mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+    # Mean Absolute Scaled Error (MASE)
+    naive_forecast = np.abs(np.diff(y_true)).mean()
+    mase = np.mean(np.abs(y_true - y_pred)) / naive_forecast if naive_forecast != 0 else np.inf
 
     return {
         "RMSE": rmse,
         "R2": r2,
-        "MAPE": mape
+        "MASE": mase
     }
