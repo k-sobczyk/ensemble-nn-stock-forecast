901ba3647b48dcb099f7dae6dc20c6ca073378b4
ksobczyk_R2DS <ksobczyk@student.agh.edu.pl>
2025-06-23 22:30:43 +0200

    datasets creation for testing

--- src/datasets/datasets_for_model.py
+++ src/datasets/datasets_for_model.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 def prepare_model_data():
     df = pd.read_csv('data/data_with_features.csv')
-
+    print(df.columns)
     # One Hot Encoding
-    df = (pd.get_dummies(df, columns=['sector'])
-            .drop(columns=['file_name']))
+    # df = (pd.get_dummies(df, columns=['sector'])
+    #         .drop(columns=['file_name']))
 
     df.to_csv('data/processed/model_with_features.csv', index=False)
 
